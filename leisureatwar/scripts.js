c56b5b0e7388b070dc6b609da2d71a69390d6f84
Spencer <srober26@gmu.edu>
2013-04-22 02:01:48 -0400

    extensive changes made

--- leisureatwar/scripts.js
+++ leisureatwar/scripts.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 $(".nav li.mobile").hide();
-$(".nav ul.mobile").click(function(){$(".nav li.mobile").toggle()});
-
-
+$(".nav ul.mobile").click(function(){
+	$(".nav li.mobile").toggle()
+	});
+//$(".letter").cycle({
+//	fit: 0,
+//	timeout: 1000
+//	});
+//$(".pause").click(function(){
+//	$(".letter").cycle('toggle')
+//	});
 });
 
