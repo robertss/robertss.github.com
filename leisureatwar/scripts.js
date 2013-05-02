76666dfc9ca16d517225e1c68092ddf9fcc148ee
Spencer <srober26@gmu.edu>
2013-05-02 17:45:11 -0400

    images improved, music added, letter pages template

--- leisureatwar/scripts.js
+++ leisureatwar/scripts.js
@@ -3,16 +3,5 @@ $(".nav li.mobile").hide();
 $(".nav ul.mobile").click(function(){
 	$(".nav li.mobile").toggle()
 	});
-
-$('#letter-show') 
-.before('<div id="letter-nav">') 
-.cycle({ 
-    fx:     'turnDown', 
-    speed:  'fast', 
-    timeout: 0, 
-    pager:  '#letter-nav' 
-})
-//.after('</div>');
-
 });
 
