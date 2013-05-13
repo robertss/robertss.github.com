49a13b4fd100f8800f2ce11a876be9edcb88fbdb
Spencer <srober26@gmu.edu>
2013-05-13 18:06:23 -0400

    extensive changes
    letters added, letter dropdown nav added for small sizes, return to top
    added for letters, etc

--- leisureatwar/scripts.js
+++ leisureatwar/scripts.js
@@ -3,5 +3,10 @@ $(".nav li.mobile").hide();
 $(".nav ul.mobile").click(function(){
 	$(".nav li.mobile").toggle()
 	});
+$("#letter-dropnav").hide();
+$("#letter-drop").click(function(){
+	$("#letter-dropnav").toggle()
+	});
+	
 });
 
