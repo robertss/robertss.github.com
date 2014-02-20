358830291118fb1e6c8273c51ca5b034c2beff23
Spencer <srober26@gmu.edu>
2014-02-20 15:04:42 -0500

    PHP
    Attempt to add PhP nav to HTML pages

---
+++ leisureatwar/phpincludes/nav-1945.php
@@ -0,0 +1,160 @@
+<div id="nav-1945">
+<ul class="secondary">
+
+
+<li<?php if ($thisPage=="45-01-14") 
+echo " class=\"curr\""; ?>><a href="45-01-14.html">January 14, 1945</a></li>
+
+<li<?php if ($thisPage=="45-01-17") 
+echo " class=\"curr\""; ?>><a href="45-01-17.html">January 17, 1945</a></li>
+
+<li<?php if ($thisPage=="45-01-18") 
+echo " class=\"curr\""; ?>><a href="45-01-18.html">January 18, 1945</a></li>
+
+<li<?php if ($thisPage=="45-01-19") 
+echo " class=\"curr\""; ?>><a href="45-01-19.html">January 19, 1945</a></li>
+
+<li<?php if ($thisPage=="45-01-20") 
+echo " class=\"curr\""; ?>><a href="45-01-20.html">January 20, 1945</a></li>
+
+<li<?php if ($thisPage=="45-01-24") 
+echo " class=\"curr\""; ?>><a href="45-01-24.html">January 24, 1945</a></li>
+
+<li<?php if ($thisPage=="45-01-26") 
+echo " class=\"curr\""; ?>><a href="45-01-26.html">January 26, 1945</a></li>
+
+<li<?php if ($thisPage=="45-01-28") 
+echo " class=\"curr\""; ?>><a href="45-01-28.html">January 28, 1945</a></li>
+
+<li<?php if ($thisPage=="45-01-30") 
+echo " class=\"curr\""; ?>><a href="45-01-30.html">January 30, 1945</a></li>
+
+<li<?php if ($thisPage=="45-02-02") 
+echo " class=\"curr\""; ?>><a href="45-02-02.html">February 2, 1945</a></li>
+
+<li<?php if ($thisPage=="45-02-04") 
+echo " class=\"curr\""; ?>><a href="45-02-04.html">February 4, 1945</a></li>
+
+<li<?php if ($thisPage=="45-02-08") 
+echo " class=\"curr\""; ?>><a href="45-02-08.html">February 8, 1945</a></li>
+
+<li<?php if ($thisPage=="45-02-09-1") 
+echo " class=\"curr\""; ?>><a href="45-02-09-1.html">#1 February 9, 1945</a></li>
+
+<li<?php if ($thisPage=="45-02-09-2") 
+echo " class=\"curr\""; ?>><a href="45-02-09-2.html">#2 February 9, 1945</a></li>
+
+<li<?php if ($thisPage=="45-02-11") 
+echo " class=\"curr\""; ?>><a href="45-02-11.html">February 11, 1945</a></li>
+
+<li<?php if ($thisPage=="45-02-13-1") 
+echo " class=\"curr\""; ?>><a href="45-02-13-1.html">#1 February 13, 1945</a></li>
+
+<li<?php if ($thisPage=="45-02-13-2") 
+echo " class=\"curr\""; ?>><a href="45-02-13-2.html">#2 February 13, 1945</a></li>
+
+<li<?php if ($thisPage=="45-02-14") 
+echo " class=\"curr\""; ?>><a href="45-02-14.html">February 14, 1945</a></li>
+
+<li<?php if ($thisPage=="45-02-20") 
+echo " class=\"curr\""; ?>><a href="45-02-20.html">February 20, 1945</a></li>
+
+<li<?php if ($thisPage=="45-02-23") 
+echo " class=\"curr\""; ?>><a href="45-02-23.html">February 23, 1945</a></li>
+
+<li<?php if ($thisPage=="45-02-27") 
+echo " class=\"curr\""; ?>><a href="45-02-27.html">February 27, 1945</a></li>
+
+<li<?php if ($thisPage=="45-03-16") 
+echo " class=\"curr\""; ?>><a href="45-03-16.html">March 16, 1945</a></li>
+
+<li<?php if ($thisPage=="45-03-27") 
+echo " class=\"curr\""; ?>><a href="45-03-27.html">March 27, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-08") 
+echo " class=\"curr\""; ?>><a href="45-04-08.html">April 8, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-09-1") 
+echo " class=\"curr\""; ?>><a href="45-04-09-1.html">#1 April 9, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-09-2") 
+echo " class=\"curr\""; ?>><a href="45-04-09-2.html">#2 April 9, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-10-1") 
+echo " class=\"curr\""; ?>><a href="45-04-10-1.html">#1 April 10, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-10-2") 
+echo " class=\"curr\""; ?>><a href="45-04-10-2.html">#2 April 10, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-11") 
+echo " class=\"curr\""; ?>><a href="45-04-11.html">April 11, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-12") 
+echo " class=\"curr\""; ?>><a href="45-04-12.html">April 12, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-13") 
+echo " class=\"curr\""; ?>><a href="45-04-13.html">April 13, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-17") 
+echo " class=\"curr\""; ?>><a href="45-04-17.html">April 17, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-20-1") 
+echo " class=\"curr\""; ?>><a href="45-04-20-1.html">#1 April 20, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-20-2") 
+echo " class=\"curr\""; ?>><a href="45-04-20-2.html">#2, April 20, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-21") 
+echo " class=\"curr\""; ?>><a href="45-04-21.html">April 21, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-22-1") 
+echo " class=\"curr\""; ?>><a href="45-04-22-1.html">#1 April 22, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-22-1") 
+echo " class=\"curr\""; ?>><a href="45-04-22-2.html">#2 April 22, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-26") 
+echo " class=\"curr\""; ?>><a href="45-04-26.html">April 26, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-27") 
+echo " class=\"curr\""; ?>><a href="45-04-27.html">April 27, 1945</a></li>
+
+<li<?php if ($thisPage=="45-04-29") 
+echo " class=\"curr\""; ?>><a href="45-04-29.html">April 29, 1945</a></li>
+
+<li<?php if ($thisPage=="45-05-02") 
+echo " class=\"curr\""; ?>><a href="45-05-02.html">May 2, 1945</a></li>
+
+<li<?php if ($thisPage=="45-05-03") 
+echo " class=\"curr\""; ?>><a href="45-05-03.html">May 3, 1945</a></li>
+
+<li<?php if ($thisPage=="45-05-04") 
+echo " class=\"curr\""; ?>><a href="45-05-04.html">May 4, 1945</a></li>
+
+<li<?php if ($thisPage=="45-05-06-1") 
+echo " class=\"curr\""; ?>><a href="45-05-06-1.html">#1 May 6, 1945</a></li>
+
+<li<?php if ($thisPage=="45-05-06-2") 
+echo " class=\"curr\""; ?>><a href="45-05-06-2.html">#2 May 6, 1945</a></li>
+
+<li<?php if ($thisPage=="45-05-08") 
+echo " class=\"curr\""; ?>><a href="45-05-08.html">May 8, 1945</a></li>
+
+<li<?php if ($thisPage=="45-05-09-1") 
+echo " class=\"curr\""; ?>><a href="45-05-09-1.html">#1 May 9, 1945</a></li>
+
+<li<?php if ($thisPage=="45-05-09-2") 
+echo " class=\"curr\""; ?>><a href="45-05-09-2.html">#2 May 9, 1945</a></li>
+
+<li<?php if ($thisPage=="45-05-11") 
+echo " class=\"curr\""; ?>><a href="45-05-11.html">May 11, 1945</a></li>
+
+<li<?php if ($thisPage=="45-05-12") 
+echo " class=\"curr\""; ?>><a href="45-05-12.html">May 12, 1945</a></li>
+
+<li<?php if ($thisPage=="45-05-16") 
+echo " class=\"curr\""; ?>><a href="45-05-16.html">May 16, 1945</a></li>
+
+</ul>
+
+</div>
--- leisureatwar/scripts.js
+++ leisureatwar/scripts.js
@@ -3,9 +3,25 @@ $(".nav li.mobile").hide();
 $(".nav ul.mobile").click(function(){
 	$(".nav li.mobile").toggle()
 	});
+
 $("#letter-dropnav").hide();
 $("#letter-drop").click(function(){
 	$("#letter-dropnav").toggle()
+	});
+
+$("#y43-dropnav").hide();
+$("#y43-drop").click(function(){
+	$("#y43-dropnav").toggle()
+	});
+
+$("#y44-dropnav").hide();
+$("#y44-drop").click(function(){
+	$("#y44-dropnav").toggle()
+	});
+
+$("#y45-dropnav").hide();
+$("#y45-drop").click(function(){
+	$("#y45-dropnav").toggle()
 	});
 	
 });
